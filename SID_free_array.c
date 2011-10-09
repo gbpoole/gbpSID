86a8db5ac9c108ea25bb66d4d34f769a28fbf2c6
Gregory Poole <gbpoole@gmail.com>
2011-10-09 23:50:18 +1100

    Debug SID_free_array() and SID_malloc_array().

--- SID_free_array.c
+++ SID_free_array.c
@@ -5,29 +5,36 @@
 #include <gbpCommon.h>
 #include <gbpSID.h>
 
-void SID_free_array(void **ptr,int n_D,...){
+void SID_free_array_local_recursive(void **ptr,int n_D,size_t *D);
+void SID_free_array_local_recursive(void **ptr,int n_D,size_t *D){
   int       i_D;
   int       n_D_pass;
-  size_t    D_i;
-  va_list   vargs;
 
-  va_start(vargs,n_D);
-
-  if((*ptr)!=NULL){
-
-    // Loop over dimensions
-    D_i=(size_t)va_arg(vargs,size_t);
-    if(n_D==1)
-      SID_free(SID_FARG ptr);
-    else if(n_D>1){
-      n_D_pass=n_D-1;
-      for(i_D=0;i_D<D_i;i_D++)
-        SID_free_array(ptr[i_D],n_D_pass,vargs);
-      SID_free(SID_FARG ptr);
-    }
-    else
-      SID_trap_error("Dimension size %d<0 in SID_free_array",ERROR_LOGIC,D_i);
+  if(D[0]<0)
+    SID_trap_error("Size of dimension %zd<0 in SID_free_array",ERROR_LOGIC,D[0]);
+  if(n_D==1)
+    SID_free(ptr);
+  else if(n_D>1){
+    n_D_pass=n_D-1;
+    for(i_D=0;i_D<D[0];i_D++)
+      SID_free_array_local_recursive(SID_FARG ptr[i_D],n_D_pass,&(D[1]));
+    SID_free(ptr);
   }
+  else
+    SID_trap_error("Number of dimensions %d<0 in SID_free_array",ERROR_LOGIC,n_D);
+}
+
+void SID_free_array(void **ptr,int n_D,...){
+  int       i_D;
+  size_t   *D;
+  va_list   vargs;
+  va_start(vargs,n_D);
+  D=(size_t *)SID_malloc(sizeof(size_t)*n_D);
+  for(i_D=0;i_D<n_D;i_D++){
+    D[i_D]=(size_t)va_arg(vargs,size_t);
+  }
+  SID_free_array_local_recursive(ptr,n_D,D);
+  SID_free(SID_FARG D);
   va_end(vargs);
 }
 
--- SID_malloc_array.c
+++ SID_malloc_array.c
@@ -5,48 +5,40 @@
 #include <gbpCommon.h>
 #include <gbpSID.h>
 
-void *SID_malloc_array(size_t allocation_size_i,int n_D,...){
+void *SID_malloc_array_local_recursive(size_t allocation_size_i,int n_D,size_t *D);
+void *SID_malloc_array_local_recursive(size_t allocation_size_i,int n_D,size_t *D){
   int       i_D;
   int       n_D_pass;
-  size_t    D_i;
-  size_t    count;
-  size_t    allocation_size;
-  char     *ptr_temp;
-  char     *ptr_return;
-  va_list   vargs;
+  void     *ptr_return;
 
-  // Count the number of elements to allocate
-  va_start(vargs,n_D);
-  for(i_D=0,count=0;i_D<n_D;i_D++)
-    count+=(size_t)va_arg(vargs,size_t);
-
-  // Set the total allocation size
-  if(n_D==1)
-    allocation_size=allocation_size_i*count;
-  else
-    allocation_size=sizeof(char *)*count;
-
-  if(allocation_size>0){
-
-    // Rewind argument counter
-    va_start(vargs,n_D);
-
-    // Loop over dimensions
-    D_i=(size_t)va_arg(vargs,size_t);
-    if(n_D==1)
-      ptr_return=SID_malloc(allocation_size_i*D_i);
-    else if(n_D>1){
-      ptr_return=SID_malloc(sizeof(char *)*D_i);
-      n_D_pass=n_D-1;
-      for(i_D=0;i_D<D_i;i_D++)
-        ((void **)ptr_return)[i_D]=SID_malloc_array(allocation_size_i,n_D_pass,vargs);
-    }
-    else
-      SID_trap_error("Dimension size %d<0 in SID_malloc_array",ERROR_LOGIC,D_i);
+  if(D[0]<0)
+    SID_trap_error("Size of dimension %zd<0 in SID_malloc_array",ERROR_LOGIC,D[0]);
+  if(n_D==1){
+    ptr_return=SID_malloc(allocation_size_i*D[0]);
+  }
+  else if(n_D>1){
+    ptr_return=SID_malloc(sizeof(char *)*D[0]);
+    n_D_pass=n_D-1;
+    for(i_D=0;i_D<D[0];i_D++)
+      ((void **)ptr_return)[i_D]=SID_malloc_array_local_recursive(allocation_size_i,n_D_pass,&(D[1]));
   }
   else
-    ptr_return=NULL;
+    SID_trap_error("Number of dimensions %d<0 in SID_malloc_array",ERROR_LOGIC,n_D);
+  return(ptr_return);
+}
+
+void *SID_malloc_array(size_t allocation_size_i,int n_D,...){
+  int       i_D;
+  size_t   *D;
+  void     *ptr_return;
+  va_list   vargs;
+  va_start(vargs,n_D);
+  D=(size_t *)SID_malloc(sizeof(size_t)*n_D);
+  for(i_D=0;i_D<n_D;i_D++)
+    D[i_D]=(size_t)va_arg(vargs,size_t);
   va_end(vargs);
+  ptr_return=SID_malloc_array_local_recursive(allocation_size_i,n_D,D);
+  SID_free(SID_FARG D);
   return(ptr_return);
 }
 
