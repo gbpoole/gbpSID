bde953b722cc111abd3c9bae67c2e5edcfabcda5
Gregory Poole <gbpoole@gmail.com>
2012-02-04 16:54:40 +1100

    Change the order of storage for triaxiality eigenvectors.

--- SID_Bcast.c
+++ SID_Bcast.c
@@ -5,15 +5,16 @@
 
 void SID_Bcast(void *buffer,int data_size,int source_rank,SID_Comm *comm){
 #if USE_MPI
-    int size_left,size_offset,size_used,size_send;
-    size_left  =data_size;
-    size_offset=0;
-    while(size_left>0){
-      size_send=MIN(size_left,MAX_SEND_LOCAL);
-      MPI_Bcast(&(((char *)buffer)[size_offset]),(int)(size_send),MPI_BYTE,source_rank,(MPI_Comm)(comm->comm));
-      size_left  -=size_send;
-      size_offset+=size_send;
-    }
+//    int size_left,size_offset,size_used,size_send;
+//    size_left  =data_size;
+//    size_offset=0;
+//    while(size_left>0){
+//      size_send=MIN(size_left,MAX_SEND_LOCAL);
+//      MPI_Bcast(&(((char *)buffer)[size_offset]),(int)(size_send),MPI_BYTE,source_rank,(MPI_Comm)(comm->comm));
+//      size_left  -=size_send;
+//      size_offset+=size_send;
+//    }
+   MPI_Bcast(buffer,data_size,MPI_BYTE,source_rank,(MPI_Comm)(comm->comm));
 #endif
 }
 
