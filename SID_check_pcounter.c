9567e4219e31e1abd12308534e55ce6bfccdb9a1
Gregory Poole <gbpoole@gmail.com>
2014-06-17 12:27:20 +1000

    Fixed a bug in the pcounter code.

--- SID_check_pcounter.c
+++ SID_check_pcounter.c
@@ -5,9 +5,9 @@
 void SID_check_pcounter(pcounter_info *pcounter,
                         size_t         i){
   if(i>=pcounter->i_report_next){
-     SID_log("%3d%% complete.",SID_LOG_COMMENT|SID_LOG_TIMER,(int)((100./(float)pcounter->n_report)*(float)(pcounter->i_report)));
-     pcounter->i_report_next=MIN(pcounter->n_i,(size_t)((float)(pcounter->n_i-i)/(float)(pcounter->n_report-pcounter->i_report)));
      pcounter->i_report++;
+     SID_log("%3d%% complete.",SID_LOG_COMMENT|SID_LOG_TIMER,(int)(100.*(((float)i)/(float)(pcounter->n_i))));
+     pcounter->i_report_next=MIN(pcounter->n_i,i+(size_t)((float)(pcounter->n_i-i)/(float)(pcounter->n_report-pcounter->i_report)));
   }
 }
 
