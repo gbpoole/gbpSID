bb697657cd8998424f054b653d6a351316744b4e
Gregory Poole <gbpoole@gmail.com>
2017-11-17 17:29:47 +1100

    Adjusted SID_test.c to address a TravisCI build error under linux.

--- src/development/SID_test.c
+++ src/development/SID_test.c
@@ -7,14 +7,16 @@ void SID_test(int val, char *fmt, ...) {
     int     i;
     va_list vargs;
     va_start(vargs, fmt);
-    fprintf(SID.fp_log, "");
+    fprintf(SID.fp_log, "\n");
 #if USE_MPI
     fprintf(SID.fp_log, "SID_test N=%05d R=%05d ", val, SID.My_rank);
 #else
     fprintf(SID.fp_log, "SID_test N=%05d ", val);
 #endif
-    vfprintf(SID.fp_log, fmt, vargs);
-    fprintf(SID.fp_log, "\n");
+    if(fmt != NULL){
+        vfprintf(SID.fp_log, fmt, vargs);
+        fprintf(SID.fp_log, "\n");
+    }
     SID_Barrier(SID.COMM_WORLD);
     va_end(vargs);
 }
