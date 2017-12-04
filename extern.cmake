1bd580d8351eaf5b5221f816c506e70b71a3d36e
Gregory Poole <gbpoole@gmail.com>
2017-12-04 14:56:03 +1100

    Now using the 'gbpBuild' project to manage this project's development environment.

--- extern.cmake
+++ extern.cmake
@@ -1,12 +1,16 @@
-# Clara is a 3rd party header-only library, by Phil Nash 
-#
-# It is used in the Catch2 project: https://github.com/catchorg/Catch2
-# and is a "simple to use, composable, command line parser for
-# C++ 11 and beyond".  See the following links for more info:
-#
-#    https://github.com/philsquared/Clara
-#    https://www.youtube.com/watch?v=Od4bjLfwI-A
-#
+# Configure any external projects, submodules, etc.
 macro(local_extern cur_dir )
+    # gbpBuild provides the boilerplate for this project's development environment
+    add_external_submodule( ${cur_dir} "gbpBuild" ${cur_dir}"/gbpBuild/README.md" )
+
+    # Clara is a 3rd party header-only library, by Phil Nash 
+    #
+    # It is used in the Catch2 project: https://github.com/catchorg/Catch2
+    # and is a "simple to use, composable, command line parser for
+    # C++ 11 and beyond".  See the following links for more info:
+    #
+    #    https://github.com/philsquared/Clara
+    #    https://www.youtube.com/watch?v=Od4bjLfwI-A
+    #
     add_external_submodule( ${cur_dir} "Clara" ${cur_dir}"/extern/Clara/include/clara.hpp" )
 endmacro()
---
+++ project.cmake
@@ -0,0 +1,25 @@
+#========= Set project-specific options ========
+# Use double precision liberally ?
+option(USE_DOUBLE "Use double liberally" OFF)
+if(USE_DOUBE)
+    add_definitions(-DUSE_DOUBLE)
+endif()
+
+# Turn on debugging mode
+option(SID_DEBUG "Enable SID debugging information" OFF)
+
+#=========== Add 3rd-party libraries ===========
+# (look in gbpBuild/cmake/3rd_party.cmake for a list of supported libraries)
+message(STATUS "")
+message(STATUS "Initializing 3rd-party libraries...")
+
+# List of *requested* 3rd-Party libraries (and their defaults)
+set_3rd_party_requested("GBP_DOCS_BUILD" ON)
+
+# List of *optional* 3rd-Party libraries (and their defaults)
+set_3rd_party_optional("MPI"    ON)
+set_3rd_party_optional("MPI_IO" OFF)
+set_3rd_party_optional("CUDA"   OFF)
+
+# Print status message
+message(STATUS "Finished initializing 3rd-party libraries.")
