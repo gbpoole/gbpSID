d6cb66631b6da1303d30c826e9e5e98dbe6a1a8b
Gregory Poole <gbpoole@gmail.com>
2018-05-22 14:23:24 +1000

    Fixed an unfixable (at least, without changing the API) UB compile problem with the std::string overloaded SID_log functions.  Removed them.

--- src/gbpSID.hh
+++ src/gbpSID.hh
@@ -2,15 +2,6 @@
 #define GBPSID_HH
 
 // Include ANSI-C headers
-#include <core/gbpSID_core.h>
-#include <logging/gbpSID_logging.h>
-#include <malloc/gbpSID_malloc.h>
-#include <file_io/gbpSID_file_io.h>
-#include <mpi/gbpSID_mpi.h>
-#include <dev/gbpSID_dev.h>
-#include <tools/gbpSID_tools.h>
-
-// Include C++ headers
-#include <logging/gbpSID_logging.hh>
+#include <gbpSID.h>
 
 #endif
--- src/logging/SID_log_warning.cc
+++
@@ -1,9 +0,0 @@
-#include <gbpSID.hh>
-
-// This is an overload of SID_log_warning() for C++ strings
-void SID_log_warning(const std::string &fmt, int mode, ...) {
-    va_list vargs;
-    va_start(vargs, mode);
-
-    SID_log_warning(fmt.c_str(),mode,vargs);
-}
